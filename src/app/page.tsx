81f281dba3a0761bae70d3c4d22598ebfebb800e
margaux-works <margaux.espinasse@gmail.com>
2025-02-08 14:58:58 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen bg-background">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind CSS is working!
+      </h1>
+    </main>
+  );
+}
